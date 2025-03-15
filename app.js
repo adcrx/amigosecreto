71f1ea0b40774bb087509493a575c95e3aabb9a6
adcrx <alejandra.cotroneo@gmail.com>
2025-03-15 16:23:58 -0300

    Creación array amigos y función agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value;
+    
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    actualizarListaAmigos();
+    input.value = "";
+}
